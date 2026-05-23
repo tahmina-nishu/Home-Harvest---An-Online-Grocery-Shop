74f3830021b6de218a63d21ec0d6feee5258dfbc
tahmina-nishu <akthertahmina572@gmail.com>
2026-05-23 09:58:28 +0600

    added react router (path)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h3 className='text-6xl text-amber-900'>Hii</h3>
+      <h3 className='text-7xl text-amber-900'>Hii</h3>
+      <h3 className='text-7xl text-amber-900'>Hii</h3>
     </div>
   )
 }
