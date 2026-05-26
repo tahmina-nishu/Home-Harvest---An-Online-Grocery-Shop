03c87464dd5d949cc2def260821fcbcf30dacbd7
tahmina-nishu <akthertahmina572@gmail.com>
2026-05-26 20:31:21 +0600

    Added Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
-      <h3 className='text-7xl text-amber-900'>Hii</h3>
-      <h3 className='text-7xl text-amber-900'>Hii</h3>
+      <Navbar></Navbar>
     </div>
   )
 }
