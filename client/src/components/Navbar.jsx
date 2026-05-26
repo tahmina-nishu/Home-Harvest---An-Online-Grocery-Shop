1b799cfda1250bc4d834488d2beeaef0414259dd
tahmina-nishu <akthertahmina572@gmail.com>
2026-05-26 21:48:09 +0600

    completed navbar (design)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
     return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink to='/'>
+            <NavLink to='/' onClick={()=> setOpen(false)}>
                 <img className='h-12' src={assets.logo} alt="logo" />
             </NavLink>
 
@@ -37,7 +37,7 @@ const Navbar = () => {
                     </svg>
                 </div>
 
-                <div className="relative cursor-pointer">
+                <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
                     <svg width="28" height="28" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#2d753c" stroke-linecap="round" stroke-linejoin="round" />
                     </svg>
