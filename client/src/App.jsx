7924fb7eb6c94d705026d4e68c9879c9f5e5d426
tahmina-nishu <akthertahmina572@gmail.com>
2026-05-29 20:59:13 +0600

    added login & sign up form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,20 @@ import Home from './pages/Home'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import {Toaster} from "react-hot-toast";
 import Footer from './components/Footer';
+import { useAppContext } from './context/AppContext';
+import Login from './components/Login';
 
 
 const App = () => {
 
   const isSellerPath = useLocation().pathname.includes("seller")
+  const {showUserLogin} = useAppContext()
+  
   return (
     <div>
 
       {isSellerPath ? null : <Navbar></Navbar>}
+      {showUserLogin ? <Login></Login> : null}
 
     <Toaster></Toaster>
 
