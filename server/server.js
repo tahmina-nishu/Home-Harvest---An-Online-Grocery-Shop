f67e5569ee63f18042847aca743250117c866caa
tahmina-nishu <akthertahmina572@gmail.com>
2026-06-05 22:15:07 +0600

    created folder structure for backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+// Allow multiple origins
+const allowedOrigins = ['http://localhost:5173'] // ekhane 1 tar beshi link o add kora jabe jeta te backend run hobe
+
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, Credentials: true}));
 
 app.get('/', (req, res)=> res.send("API is working"));
 
