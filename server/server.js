363283fcafb21c138db5e3a665ab446f2fb9f07f
tahmina-nishu <akthertahmina572@gmail.com>
2026-06-08 13:24:08 +0600

    successfully connected the express project with mongoDB database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
+import connectDB from './configs/db.js';
+import 'dotenv/config'
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+await connectDB()
 
 // Allow multiple origins
 const allowedOrigins = ['http://localhost:5173'] // ekhane 1 tar beshi link o add kora jabe jeta te backend run hobe
