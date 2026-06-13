544fb60f6aacd48205a4f1cf1bd9bcfd0bfd1575
tahmina-nishu <akthertahmina572@gmail.com>
2026-06-13 15:48:36 +0600

    created user controller function for authenticating user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -115,3 +115,25 @@ export const login = async (req, res)=>{
         });
     }
 }
+
+// Check Auth : /api/user/is-auth
+export const isAuth = async (req, res)=>{
+    try {
+        const {userId} = req.body;
+
+        // find the user from database and rmove the password data
+        const user = await User.findById(userId).select("-password")
+        return res.json({
+                success: true, 
+                user
+            })
+
+            
+    } catch (error) {
+        console.log(error.message);
+        res.json({
+            success: false, 
+            message: error.message
+        });
+    }
+}
---
+++ server/middleWares/authUser.js
@@ -0,0 +1,35 @@
+
+
+const authUser = async (req, res, next)=>{
+    const {token} = req.cookies;
+
+    if(!token){
+        return res.json({
+            success: false,
+            message: 'Not Authorized'
+        });
+    }
+
+    // token available na hole if condition ta kaj korbe . r available hole try catch kaj korbe
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
+        if(tokenDecode.id){
+            req.body.userId = tokenDecode.id;
+        }else{
+            return res.json({
+                success: false,
+                message: 'Not Authorized'
+            });
+        }
+
+        next();
+
+    } catch (error) {
+        res.json({
+                success: false,
+                message: error.message
+            });
+    }
+}
+
+export default authUser
