7ee5558dfee642c8520d887aa38fda9b1aca22f9
tahmina-nishu <akthertahmina572@gmail.com>
2026-06-16 09:41:55 +0600

    added seller router in main server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config'
 import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -20,6 +21,7 @@ app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.get('/', (req, res)=> res.send("API is working"));
 app.use('/api/user', userRouter)
+app.use('/api/seller', sellerRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
