3d647b0017e6bbc1992ca538b2f00e5205dc3603
tahmina-nishu <akthertahmina572@gmail.com>
2026-06-16 09:32:04 +0600

    created seller logout function

--- server/controllers/sellerController.js
+++ server/controllers/sellerController.js
@@ -54,3 +54,28 @@ export const isSellerAuth = async (req, res)=>{
         });
     }
 }
+
+// Logout Seller : /api/seller/logout
+export const sellerLogout = async (req, res)=>{
+    try {
+        // clear the cookie
+        res.clearCookie('sellerToken', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production', 
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+        })
+
+        // cookie clear korar por logged out hoye jabe. erpor ekta response pathabe
+        return res.json({
+                success: true, 
+                message: "Logged Out"
+            })
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({
+            success: false, 
+            message: error.message
+        });
+    }
+}
