63f1a79a252d2b4ec0b8aafd306542399e741127
tahmina-nishu <akthertahmina572@gmail.com>
2026-06-27 22:03:52 +0600

    managed inStock property in product list

--- client/src/pages/seller/ProductList.jsx
+++ client/src/pages/seller/ProductList.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import {useAppContext} from '../../context/AppContext'
+import toast from 'react-hot-toast';
 
 const ProductList = () => {
     
-    const {products, currency} = useAppContext();
+    const {products, currency, axios, fetchProducts} = useAppContext();
+
+    const toggleStock = async (id, inStock) => {
+        try {
+            const {data} = await axios.post('/api/product/stock', {id, inStock});
+
+            if(data.success){
+                fetchProducts();
+                toast.success(data.message)
+            }else{
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }      
+    }
 
     return (
         <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll flex flex-col justify-between pb-14">
@@ -32,7 +49,7 @@ const ProductList = () => {
                                     <td className="px-4 py-3 max-sm:hidden">{currency}{product.offerPrice}</td>
                                     <td className="px-4 py-3">
                                         <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
-                                            <input type="checkbox" className="sr-only peer"/>
+                                            <input onClick={()=> toggleStock(product._id, !product.inStock)}  checked={product.inStock} type="checkbox" className="sr-only peer"/>
 
                                             {/* instock property add kore jegula stock e ache egula auto on thakbe. er jonno uporer input ta eta diye replace korbo...
 
