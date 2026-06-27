c91d338bd3b2da672baa5239c1c8f1fdaf7f339d
tahmina-nishu <akthertahmina572@gmail.com>
2026-06-28 01:41:39 +0600

    managed cart functionalities (add to cart & update quantity) & add cartItems in db

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -137,6 +137,27 @@ export const AppContextProvider = ({children})=>{
         fetchProducts()
     },[])
 
+    // Update database cart items
+    useEffect(()=>{
+        // api call
+        const updateCart = async ()=>{
+            try {
+                const {data} = await axios.post('/api/cart/update', {cartItems})
+
+                if(!data.success){
+                    toast.error(data.message)
+                }
+            } catch (error) {
+                toast.error(error.message)
+            }
+        }
+
+        if(user){
+            updateCart()
+        }
+        
+    },[cartItems])
+
 
     const value = {
         navigate, 
