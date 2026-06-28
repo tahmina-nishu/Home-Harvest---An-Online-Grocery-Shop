40dd0d79c83f529517caa8b8f9397f884dc5baac
tahmina-nishu <akthertahmina572@gmail.com>
2026-06-28 21:40:05 +0600

    managed my order page for customer

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -5,16 +5,25 @@ import { dummyOrders } from '../assets/assets'
 const MyOrders = () => {
 
     const [myOrders, setMyOrders] = useState([])
-    const {currency} = useAppContext()
+    const {currency, axios, user} = useAppContext()
 
     // function for fetch my orders
     const fetchMyOrders = async ()=>{
-        setMyOrders(dummyOrders)
+        try {
+            const {data} = await axios.get('/api/order/user')
+            if(data.success){
+                setMyOrders(data.orders)
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     useEffect(()=>{
-        fetchMyOrders()
-    },[])
+        if(user){
+            fetchMyOrders()
+        }
+    },[user])
 
     return (
         <div className="mt-29 pb-16">
